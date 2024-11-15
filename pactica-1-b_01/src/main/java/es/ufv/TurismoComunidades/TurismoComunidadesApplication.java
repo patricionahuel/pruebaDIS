37b06a5743b84410de29c300cc68d6da40455532
patricionahuel <pncs_1195@hotmail.com>
2024-11-15 12:25:25 +0100

    menu vacio creado

---
+++ pactica-1-b_01/src/main/java/es/ufv/TurismoComunidades/TurismoComunidadesApplication.java
@@ -0,0 +1,56 @@
+package es.ufv.TurismoComunidades;
+
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TurismoComunidadesApplication {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		
+
+		int option;
+		do {
+			System.out.println("\n----- Menú Principal -----");
+			System.out.println("1. Convertir CSV a JSON");
+			System.out.println("2. Agrupar por comunidad de destino");
+			System.out.println("3. Resultados por período");
+			System.out.println("4. Resultados origen");
+			System.out.println("5. Salir");
+			System.out.print("Seleccione una opción: ");
+			option = scanner.nextInt();
+			scanner.nextLine();  // Limpiar el salto de línea
+			switch (option) {
+				case 1:
+
+					break;
+				case 2:
+
+					break;
+				case 3:
+
+
+					break;
+				case 4:
+
+					break;
+				case 5:
+					System.out.println("Saliendo del programa...");
+					break;
+				default:
+					System.out.println("Opción no válida, por favor intente de nuevo.");
+					break;
+			}
+		} while (option != 5);
+
+		scanner.close();
+	}
+
+
+}
